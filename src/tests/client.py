16c632a5ac8ce856001c0d189e59bdf2de2072fc
rodrigocam <rodrigo.redcode@gmail.com>
2019-05-28 14:07:29 -0300

    Fix client

--- src/tests/client.py
+++ src/tests/client.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
 import socket
+from getmac import get_mac_address
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+
+HOST = 'shelby-571.local'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 current_ip = socket.gethostbyname(socket.gethostname())
+mac_addr = get_mac_address(interface='eth0')
+
+print(f'my mc is {mac_addr}')
+
+msg = f'ip - {current_ip}, mac - {mac_addr}'
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as socket:
     socket.connect((HOST, PORT))
-    socket.sendall(bytes(current_ip, 'raw_unicode_escape'))
+    socket.sendall(bytes(msg, 'raw_unicode_escape'))
     data = socket.recv(1024)
 
 print('Received', repr(data))
