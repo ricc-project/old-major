2565e8ba8d02b3096a095d1352c111ccbf1b8315
rodrigocam <rodrigo.redcode@gmail.com>
2019-05-27 22:35:54 -0300

    Add initial led debugger

---
+++ src/old-major/led_debug.py
@@ -0,0 +1,56 @@
+from time import sleep
+
+import RPi.GPIO as GPIO
+
+
+# inverted high (see cathode/anode led)
+COLOR_ON = GPIO.LOW
+COLOR_OFF = GPIO.HIGH
+
+
+class LedDebugger:
+
+    def __init__(self, red_pin=2, green_pin=3, blue_pin=4):
+        self.red = red_pin
+        self.green = green_pin
+        self.blue = blue_pin
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+
+        GPIO.setup(red_pin, GPIO.OUT)
+        GPIO.setup(green_pin, GPIO.OUT)
+        GPIO.setup(blue_pin, GPIO.OUT)
+
+    async def failed(self):
+        self._blink_red()
+        sleep(1)
+        self._blink_red()
+
+    async def success(self):
+        self._blink_green()
+        sleep(1)
+        self._blink_green()
+
+    async def neutral(self):
+        self._blink_blue()
+        sleep(1)
+        self._blink_blue()
+
+    async def _blink_red(self):
+        GPIO.output(self.red, COLOR_ON)
+        GPIO.output(self.green, COLOR_OFF)
+        GPIO.output(self.blue, COLOR_OFF)
+        GPIO.cleanup()
+
+    def _blink_green(self):
+        GPIO.output(self.red, COLOR_OFF)
+        GPIO.output(self.green, COLOR_ON)
+        GPIO.output(self.blue, COLOR_OFF)
+        GPIO.cleanup()
+
+    def _blink_blue(self):
+        GPIO.output(self.red, COLOR_OFF)
+        GPIO.output(self.green, COLOR_OFF)
+        GPIO.output(self.blue, COLOR_ON)
+        GPIO.cleanup()
